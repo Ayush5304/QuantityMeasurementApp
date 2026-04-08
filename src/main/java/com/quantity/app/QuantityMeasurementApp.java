3e3874865c7d96b7be1de22af792794dfb2745d5
Ayush5304 <mishrayush0503@gmail.com>
2026-04-08 16:01:21 +0530

    [Ayush5304]

---
+++ src/main/java/com/quantity/app/QuantityMeasurementApp.java
@@ -0,0 +1,23 @@
+package com.quantity.app;
+
+import com.quantity.controller.QuantityMeasurementController;
+import com.quantity.repository.QuantityMeasurementCacheRepository;
+import com.quantity.service.IQuantityMeasurementService;
+import com.quantity.service.QuantityMeasurementServiceImpl;
+
+public class QuantityMeasurementApp {
+
+    public static void main(String[] args) {
+
+        var repository = QuantityMeasurementCacheRepository.getInstance();
+
+        IQuantityMeasurementService service =
+                new QuantityMeasurementServiceImpl(repository);
+
+        QuantityMeasurementController controller =
+                new QuantityMeasurementController(service);
+
+        controller.performAddition();
+    }
+
+}
